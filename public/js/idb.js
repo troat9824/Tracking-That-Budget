1b0a68887e97e8acbd8d4fb3ac89662332e0fe48
troat9824 <troat9824@gmail.com>
2022-05-26 05:52:48 -0500

    service-worker

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,3 +27,52 @@ request.onerror = function(event) {
     //log error here
     console.log(event.target.errorCode);
 };
+
+function saveTrans(trans) {
+    const transaction = db.transaction(['new_transaction'],'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    budgetObjectStore.add(trans);
+}
+function uploadTrans() {
+    // open a transaction on the db 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            fetch('api/transaction', {
+                method:'POST',
+                body:JSON.stringify(getAll.result),
+                headers:{
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            /then(serverResponse => {
+                if(serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+                const budgetObjectStore = transaction.objectStore('new_transaction');
+
+                budgetObjectStore.clear();
+
+                alert('All saved transactions have been submitted!');
+
+            })
+            .catch(err=> {
+                console.log(err);
+            });
+        }
+    }
+}
+
+// listen for application coming back online
+window.addEventListener('online', uploadTrans);
